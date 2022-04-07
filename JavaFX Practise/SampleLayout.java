da4ca3cdd14a4f48f66a131e36645604662f4ab7
DJ <social.deej@gmail.com>
2022-04-08 00:39:33 +0530

    feat: Sample Layout

--- JavaFX Practise/SampleLayout.java
+++ JavaFX Practise/SampleLayout.java
@@ -1,18 +1,66 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SampleLayout extends Application {
 
     @Override
-    public void start(final Stage stage) throws Exception
+    public void start(Stage stage) throws Exception
     {
-        final Circle circ = new Circle(40, 40, 30);
-        final Group root = new Group(circ);
-        final Scene scene = new Scene(root, 400, 300);
-        stage.setTitle("Hello JavaFX 2.0!");
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 600, Color.LIGHTSKYBLUE);
+
+        Text text = new Text();
+        text.setText("HELLO WORLD");
+        text.setX(50);
+        text.setY(50);
+        text.setFont(Font.font("Verdana", 50));
+        text.setFill(Color.GREEN);
+
+        Line line = new Line();
+        line.setStartX(100);
+        line.setStartY(100);
+        line.setEndX(200);
+        line.setEndY(100);
+        line.setStrokeWidth(5);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(200);
+        rectangle.setY(300);
+        rectangle.setWidth(100);
+        rectangle.setHeight(200);
+        rectangle.setFill(Color.BLUE);
+        rectangle.setStrokeWidth(5);
+        rectangle.setStroke(Color.BLACK);
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+            200.0, 200.0,
+            300.0, 300.0,
+            200.0, 300.0
+        );
+        triangle.setFill(Color.YELLOW);
+
+        Circle circle = new Circle();
+        circle.setCenterX(250);
+        circle.setCenterY(450);
+        circle.setRadius(50);
+        circle.setFill(Color.ORANGE);
+
+        root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(rectangle);
+        root.getChildren().add(triangle);
+        root.getChildren().add(circle);
+
         stage.setScene(scene);
         stage.show();
     }
